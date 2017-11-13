32c40e6ea9b4d24ff26198eca79f50f416bb255e
J2-nsung <goflrkqk@naver.com>
2017-11-14 00:07:24 +0900

    x

--- 1008.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main() {
-	double A, B;
-	scanf("%lf%lf", &A, &B);
-	printf("%.9f", A / B);
-
-	return 0;
-}
--- 11719.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-char c;
-int main() {
-	while (~scanf("%c", &c))putchar(c);
-	return 0;
-}
