34f1807e1f52b03e07490b382b7bc6699a455bd7
Jinsung <goflrkqk@naver.com>
2018-12-05 10:13:32 +0900

    study

---
+++ Problem_Solving/13_catalan.cpp
@@ -0,0 +1,20 @@
+﻿#include<cstdio>
+using namespace std;
+
+int catalan[19];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	catalan[0] = 1;
+	catalan[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			catalan[i] = catalan[i] + (catalan[j] * catalan[i - 1 - j]);
+		}
+	}
+	printf("%d\n", catalan[n]);
+
+	return 0;
+}
