445366c5c4fd1fe76a1bb03a74d375ef04b9de5c
Jinsung <goflrkqk@naver.com>
2019-09-09 18:39:25 +0900

    programmers

---
+++ Programmers/같은_숫자는_싫어.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+	for (int i = 0; i < arr.size(); i++) {
+		if (i == 0) {
+			answer.push_back(arr[i]);
+			continue;
+		}
+		if (answer.back() != arr[i]) {
+			answer.push_back(arr[i]);
+		}
+	}
+
+	return answer;
+}
