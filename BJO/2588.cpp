ccdb97bd0203cb31a90576032029e7a683645227
Jinsung <goflrkqk@naver.com>
2020-03-04 15:01:20 +0900

    BJO

---
+++ BJO/2588.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+using namespace std;
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	printf("%d\n", a*(b % 10));
+	printf("%d\n", a*((b % 100) / 10));
+	printf("%d\n", a*(b / 100));
+	printf("%d\n", a*b);
+
+	return 0;
+}
