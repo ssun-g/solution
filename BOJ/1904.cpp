1eabea6b029f4e0ac0f482b2888f2e28ef914b6b
Jinsung <goflrkqk@naver.com>
2020-04-01 23:45:57 +0900

    BOJ

--- BOJ/1904.cpp
+++ BOJ/1904.cpp
@@ -1,21 +1,17 @@
 #include<cstdio>
-#include<vector>
 using namespace std;
 
-vector<int> v;
+int arr[1000000];
 
 int main() {
 	int n;
 	scanf("%d", &n);
-	v.push_back(0);
-	v.push_back(1);
-	v.push_back(2);
-	
-	for (int i = 3; i <= n; i++) {
-		v.push_back((v[i - 2] + v[i - 1])%15746);
-	}
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i <= n; i++) arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
 
-	printf("%d\n", v[n]);
+	printf("%d\n", arr[n]);
 
 	return 0;
 }
