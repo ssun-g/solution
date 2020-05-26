9c49bf8c846e67f52681eb3e862b02185d040bc6
Jinsung <goflrkqk@naver.com>
2020-05-26 23:20:26 +0900

    BOJ

---
+++ BOJ/9372.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+using namespace std;
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+	for (int i = 0; i < tc; i++) {
+		int n, m;
+		scanf("%d%d", &n, &m);
+		for (int j = 0; j < m; j++) {
+			int a, b;
+			scanf("%d%d", &a, &b);
+		}
+		printf("%d\n", n - 1);
+	}
+
+	return 0;
+}
