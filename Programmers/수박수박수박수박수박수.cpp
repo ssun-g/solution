07ed1a3d62796c760e61bddce7cfc7f403bf2393
Jinsung <goflrkqk@naver.com>
2020-07-24 21:17:10 +0900

    programmers

---
+++ Programmers/수박수박수박수박수박수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	for (int i = 1; i <= n; i++) {
+		if (i % 2) answer += "¼ö";
+		else answer += "¹Ú";
+	}
+	return answer;
+}
