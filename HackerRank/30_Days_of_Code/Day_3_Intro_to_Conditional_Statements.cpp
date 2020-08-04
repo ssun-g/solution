9fd60ad9d2154487a8f56a38b395374eb891486b
Jinsung <goflrkqk@naver.com>
2020-08-04 21:50:08 +0900

    hackerrank

---
+++ HackerRank/30_Days_of_Code/Day_3_Intro_to_Conditional_Statements.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	int N;
+	cin >> N;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	if (N % 2 == 1) cout << "Weird" << '\n';
+	else if (N % 2 == 0) {
+		if (2 <= N && N <= 5) cout << "Not Weird" << '\n';
+		else if (6 <= N && N <= 20) cout << "Weird" << '\n';
+		else cout << "Not Weird" << '\n';
+	}
+
+	return 0;
+}
