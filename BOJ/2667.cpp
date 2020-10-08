2c5ceb3fe271070546781f3ac598bd4d046ac943
unknown <goflrkqk@naver.com>
2020-10-08 17:09:32 +0900

    BOJ

--- BOJ/2667.cpp
+++ BOJ/2667.cpp
@@ -1,50 +1,49 @@
 #include<cstdio>
-#include<algorithm>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-int n;
-int dr[4] = { 0, 0, 1, -1 };
-int dc[4] = { 1, -1, 0, 0 };
-int arr[25][25];
-int check[25][25];
+int Map[26][26];
+int check[26][26];
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
 
-int dfs(int, int);
+int dfs(int y, int x, int size) {
+	check[y][x] = 1;
+	int ret = 1;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (0 <= nx && nx < size && 0 <= ny && ny < size && !check[ny][nx] && Map[ny][nx] == 1)
+			ret += dfs(ny, nx, size);
+	}
+
+	return ret;
+}
 
 int main() {
-	vector<int> ans;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%1d", &arr[i][j]);
+	vector<int> answer;
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%1d", &Map[i][j]);
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (check[i][j] == 0 && arr[i][j] == 1) {
-				check[i][j] = 1;
-				ans.push_back(dfs(i, j));
-			}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!check[i][j] && Map[i][j] == 1)
+				answer.push_back(dfs(i, j, N));
 		}
 	}
-	sort(ans.begin(), ans.end());
-	printf("%d\n", ans.size());
-	for (int i = 0; i < ans.size(); i++) {
-		printf("%d\n", ans[i]);
+
+	int Size = answer.size();
+	printf("%d\n", Size);
+	sort(answer.begin(), answer.end());
+	for (int i = 0; i < Size; i++) {
+		printf("%d\n", answer[i]);
 	}
 
 	return 0;
-}
-
-int dfs(int r, int c) {
-	int ret = 1;
-	for (int i = 0; i < 4; i++) {
-		int nr = r + dr[i];
-		int nc = c + dc[i];
-		if (0 <= nr && nr < n && 0 <= nc && nc < n && check[nr][nc] == 0 && arr[nr][nc] == 1) {
-			check[nr][nc] = 1;
-			ret += dfs(nr, nc);
-		}
-	}
-	return ret;
 }
