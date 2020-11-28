45e94ee537dfadb51267931b732e4bc065e4746e
unknown <goflrkqk@naver.com>
2020-11-29 00:25:29 +0900

    BOJ

--- BOJ/1806.cpp
+++ BOJ/1806.cpp
@@ -1,42 +1,33 @@
-﻿#include <iostream>
-#include <algorithm>
+﻿#include<iostream>
+#include<algorithm>
 using namespace std;
+
 int arr[100001];
-const int INF = 987654321;
-int main(void)
-{
 
+int main() {
+	int answer = 987654321;
 	int N, S;
-	int length = INF;
-	int low = 0;
-	int high = 0;
-	int sum = 0;
 	cin >> N >> S;
-
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
 	}
 
-	sum = arr[0];
+	int left = 0, right = 0, sum = 0, length = 0;
+	while (left <= right) {
+		if (sum >= S) {
+			sum -= arr[left++];
+			length--;
+		}
+		else if (right == N) break;
+		else {
+			sum += arr[right++];
+			length++;
+		}
 
-	while (low <= high && high < N) {
-		if (sum < S) {
-			sum += arr[++high];
-		}
-		else if (sum == S) {
-			length = min(length, (high - low + 1));
-			sum += arr[++high];
-		}
-		else if (sum > S) {
-			length = min(length, (high - low + 1));
-			sum -= arr[low];
-			low++;
-		}
+		if (sum >= S) answer = min(answer, length);
 	}
+	if (answer == 987654321) cout << 0 << '\n';
+	else cout << answer << '\n';
 
-	if (length == INF)
-		cout << 0 << endl;
-	else
-		cout << length << endl;
 	return 0;
 }
