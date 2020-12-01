d1d7b37f677e3c989d5d203d4c11e1fa35470b48
unknown <goflrkqk@naver.com>
2020-12-01 22:08:24 +0900

    BOJ

---
+++ BOJ/2745.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>>
+#include<string>
+using namespace std;
+
+int main() {
+	string s;
+	int B;
+	int answer = 0;
+	cin >> s >> B;
+
+	int digit = 1;
+	for (int i = s.length() - 1; i >= 0; i--) {
+		if ('0' <= s[i] && s[i] <= '9') answer += digit *(s[i] - '0');
+		else answer += digit*(s[i] - 'A' + 10);
+		digit *= B;
+	}
+	cout << answer << '\n';
+
+	return 0;
+}
