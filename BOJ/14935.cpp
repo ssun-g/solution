bd2d2c2618d9a543baa7f6453a4f0f4c2414ad58
unknown <goflrkqk@naver.com>
2020-12-17 20:51:34 +0900

    boj

---
+++ BOJ/14935.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+bool isFA(string s) {
+	int tmp = (s[0] - '0') * s.length();
+	s = to_string(tmp);
+	for (int i = 0; i < 100; i++) {
+		int f = s[0] - '0';
+		int len = s.length();
+		
+		if (tmp == f*len) return true;
+		tmp = f*len;
+	}
+
+	return false;
+}
+
+int main() {
+	string s;
+	cin >> s;
+
+	if (isFA(s)) cout << "FA" << '\n';
+	else cout << "NFA" << '\n';
+	
+	return 0;
+}
