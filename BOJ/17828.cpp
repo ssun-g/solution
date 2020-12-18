71ca892427e0dc747c23a8e057715163ad8a7b8b
unknown <goflrkqk@naver.com>
2020-12-18 19:11:32 +0900

    BOJ

---
+++ BOJ/17828.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string answer = "";
+	int N, X;
+	cin >> N >> X;
+	if (N * 26 < X || X < N) cout << "!" << '\n';
+	else {
+		while (X) {
+			int diff = 26 * N;
+			if (diff - X < 26) {
+				answer += (26 - (diff - X) - 1) + 'A';
+				X -= (26 - (diff - X));
+			}
+
+			else {
+				answer += 'A';
+				X--;
+			}
+			N--;
+		}
+	}
+	cout << answer << '\n';
+
+	return 0;
+}
