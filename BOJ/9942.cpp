1ce6485b7757264f325a8fce57bc41b0f8ece9aa
unknown <goflrkqk@naver.com>
2020-12-19 16:35:56 +0900

    BOJ

---
+++ BOJ/9942.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+long long dp[1001];
+
+long long move(int n) {
+	long long ret = LLONG_MAX;
+	if (n == 1) return 1;
+
+	for (int i = 1; i < n; i++) {
+		if (n - i >= 63) continue;
+		dp[i] = (dp[i] == 0) ? move(i) : dp[i];
+		long long rMove = dp[i];
+		long long nrMove = pow(2, n - i) - 1;
+		ret = min(ret, 2 * rMove + nrMove);
+	}
+
+	return ret;
+}
+
+int main() {
+	int n, cnt = 1;
+	while (cin >> n) {
+		cout << "Case " << cnt++ << ": " << move(n) << '\n';
+	}
+
+	return 0;
+}
