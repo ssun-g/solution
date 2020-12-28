0f2cd097672b1ac4973c1d30547b1a91248c32d3
unknown <goflrkqk@naver.com>
2020-12-28 17:46:41 +0900

    BOJ

---
+++ BOJ/17509.cpp
@@ -0,0 +1,41 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+class Solve {
+public:
+	int D;
+	int V;
+
+	Solve(int D, int V) {
+		this->D = D;
+		this->V = V;
+	}
+};
+
+bool comp(Solve a, Solve b) {
+	return a.D < b.D;
+}
+
+vector<Solve> problems;
+
+int main() {
+	for (int i = 0; i < 11; i++) {
+		int d, v;
+		cin >> d >> v;
+		problems.push_back(Solve(d, v));
+	}
+	sort(problems.begin(), problems.end(), comp);
+
+
+	int D = 0;
+	int V = 0;
+	int answer = 0;
+	for (int i = 0; i < problems.size(); i++) {
+		D += problems[i].D;
+		V = problems[i].V;
+		answer += D + 20 * V;
+	}
+	cout << answer << '\n';
+
+	return 0;
+}
