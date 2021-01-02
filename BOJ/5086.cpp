6977a43451e525bd105772ce2179d82d182db875
unknown <goflrkqk@naver.com>
2021-01-02 20:09:20 +0900

    BOJ

---
+++ BOJ/5086.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+string solve(int n, int m) {
+	if (m % n == 0) return "factor";
+	else if (n%m == 0) return "multiple";
+	else return "neither";
+}
+
+int main() {
+	while (true) {
+		int n, m;
+		cin >> n >> m;
+		if (n == 0 && m == 0) break;
+		else cout << solve(n, m) << '\n';
+	}
+
+	return 0;
+}
