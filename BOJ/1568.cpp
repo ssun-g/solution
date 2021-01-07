00694a6fe227b2396610b81d9e6a29a8cd9011b4
unknown <goflrkqk@naver.com>
2021-01-07 16:46:27 +0900

    BOJ

---
+++ BOJ/1568.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int sum = 0, cnt = 0, plus = 1;
+	while (sum < N) {
+		if (sum + plus > N) {
+			plus = 1;
+			continue;
+		}
+		sum += plus;
+		plus++;
+		cnt++;
+	}
+	cout << cnt << '\n';
+
+	return 0;
+}
