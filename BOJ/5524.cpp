8968ba9bcaa32497fe1797620f8c551685d7808e
unknown <goflrkqk@naver.com>
2021-01-14 17:42:15 +0900

    BOJ

---
+++ BOJ/5524.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+string solve(string s) {
+	string ret = "";
+	for (int i = 0; i < s.length(); i++) {
+		if ('A' <= s[i] && s[i] <= 'Z') ret += tolower(s[i]);
+		else ret += s[i];
+	}
+
+	return ret;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
+		cout << solve(s) << '\n';
+	}
+
+	return 0;
+}
