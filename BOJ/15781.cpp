2e96de6867c8ec04c629e3db761da2ab3db869fa
unknown <goflrkqk@naver.com>
2021-01-15 19:29:14 +0900

    BOJ

---
+++ BOJ/15781.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int helmet[1000];
+int jacket[1000];
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> helmet[i];
+	}
+
+	for (int i = 0; i < M; i++) {
+		cin >> jacket[i];
+	}
+	sort(helmet, helmet + N);
+	sort(jacket, jacket + M);
+
+	cout << helmet[N - 1] + jacket[M - 1] << '\n';
+
+	return 0;
+}
