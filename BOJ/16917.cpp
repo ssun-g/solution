69bf9bc21a692140aea8b6342ae880efc5fdbb50
unknown <goflrkqk@naver.com>
2021-02-21 21:20:13 +0900

    BOJ

---
+++ BOJ/16917.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int result = 0;
+	int sauce, fried, halfnhalf, X, Y;
+	cin >> sauce >> fried >> halfnhalf >> X >> Y;
+
+	if (sauce + fried < 2 * halfnhalf) result += sauce*X + fried*Y;
+	else {
+		result += halfnhalf * 2 * min(X, Y);
+		if (X - Y > 0) {
+			if (sauce < 2 * halfnhalf) result += (X - Y)*sauce;
+			else result += (X - Y) * 2 * halfnhalf;
+		}
+		else if (Y - X > 0) {
+			if (fried < 2 * halfnhalf) result += (Y - X)*fried;
+			else result += (Y - X) * 2 * halfnhalf;
+		}
+	}
+	cout << result << '\n';
+
+	return 0;
+}
