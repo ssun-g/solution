f74e84b8f7b94a640a85c05a487d2c758239817f
unknown <goflrkqk@naver.com>
2021-06-24 23:59:36 +0900

    BOJ

--- BOJ/15687.cpp
+++ BOJ/15687.cpp
@@ -1,44 +1,41 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Rectangle {
-private:
-	int width, height;
+	private:
+		int width, height;
 
-public:
-	Rectangle(int width, int height) {
-		this->width = width;
-		this->height = height;
-	}
-
-	const int get_width() {
-		return this->width;
-	}
-
-	const int get_height() {
-		return this->height;
-	}
-
-	void set_width(int width) {
-		if(0 < width && width <= 1000)
+	public:
+		Rectangle(int width, int height) {
 			this->width = width;
-	}
-
-	void set_height(int height) {
-		if(0 < height && height <= 2000)
 			this->height = height;
-	}
+		}
 
-	const int area() {
-		return this->width * this->height;
-	}
+		int get_width() const {
+			return this->width;
+		}
 
-	const int perimeter() {
-		return 2 * (this->width + this->height);
-	}
+		int get_height() const {
+			return this->height;
+		}
 
-	const bool is_square() {
-		if (this->width == this->height) return true;
-		else return false;
-	}
+		void set_width(int width) {
+			if (0 < width && width <= 1000)
+				this->width = width;
+		}
+
+		void set_height(int height) {
+			if (0 < height && height <= 2000)
+				this->height = height;
+		}
+
+		int area() const {
+			return this->width * this->height;
+		}
+
+		int perimeter() const {
+			return 2 * (this->width + this->height);
+		}
+
+		bool is_square() const {
+			if (this->width == this->height) return true;
+			else return false;
+		}
 };
