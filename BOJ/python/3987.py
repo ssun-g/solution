3f8c6e71f3ae73badf01aeb6cbc9c0cd7ba2b3e9
unknown <goflrkqk@naver.com>
2021-07-23 19:19:10 +0900

    BOJ python

--- BOJ/python/3987.py
+++ BOJ/python/3987.py
@@ -1,3 +1,11 @@
+"""
+
+재채점 되어서 틀렸던 문제.
+line 18에서 항성계 전체를 도는 시간으로 탈출 조건을 걸어 두었는데
+같은 지점을 방문하더라도 전파 방향이 다르다면 다른 경우인 것을 인지하지 못해서 틀림.
+모든 방향을 고려하기 위해 간단히 *4를 해주었다.
+
+"""
 import sys
 
 input = sys.stdin.readline
@@ -7,8 +15,8 @@ def signal(y, x, direction):
     prop = 0
     while True:
         global N, M
-        if prop > N * M:
-            break  # 전파 시간이 항성계 전체를 도는 시간보다 크다면 무한루프라고 판단.
+        if prop > N * M * 4:  # 전파 시간이 항성계 전체를 도는 시간(*4는 모든 방향)보다 크다면 무한루프라고 판단.
+            break
         if (x < 0) or (y < 0) or (x >= M) or (y >= N) or (Map[y][x] == "C"):
             break  # 전파가 맵 밖으로 나가거나 블랙홀을 만난 경우
         prop += 1
@@ -70,7 +78,7 @@ answer["L"] = signal(PR, PC, "L")
 
 dict_key_max = max(answer.keys(), key=lambda x: answer[x]) # dict에서 가장 큰 value값을 가지는 key를 찾는다.
 print(dict_key_max)
-if answer[dict_key_max] > N * M:
+if answer[dict_key_max] > N * M * 4:
     print("Voyager")
 else:
     print(answer[dict_key_max])
