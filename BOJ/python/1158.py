3f7a0d04c7a4a5edbf22765373627a3eec8bf4ce
ssun-g <goflrkqk@naver.com>
2025-02-13 03:29:21 +0900

    (250213) BOJ-1158

---
+++ BOJ/python/1158.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+
+def main() -> None:
+    input = sys.stdin.readline
+
+    N, K = map(int, input().rstrip('\n').split())
+    queue = deque(list(range(1, N + 1)))
+    count = 0  # 순서 체크
+
+    print("<", end='')
+    while len(queue) != 1:
+        front = queue.popleft()  # 가장 앞 사람을 뽑는다
+        count += 1
+        if count % K == 0:  # K번째이면 제거
+            print(f"{front}, ", end='')
+        else:  # K번째가 아니면 맨 뒤로 보낸다
+            queue.append(front)
+
+    print(f"{queue[-1]}>")
+
+
+if __name__ == "__main__":
+    main()
