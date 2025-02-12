9cb9100b9567045566222de7dba39a68e66300b1
ssun-g <goflrkqk@naver.com>
2025-02-13 03:33:27 +0900

    (250213) BOJ-1158

--- BOJ/python/1158.py
+++ BOJ/python/1158.py
@@ -10,7 +10,7 @@ def main() -> None:
     count = 0  # 순서 체크
 
     print("<", end='')
-    while len(queue) != 1:
+    while len(queue) != 1:  # 출력 형식을 맞추기 위해 마지막 사람은 제거하지 않음
         front = queue.popleft()  # 가장 앞 사람을 뽑는다
         count += 1
         if count % K == 0:  # K번째이면 제거
@@ -18,7 +18,7 @@ def main() -> None:
         else:  # K번째가 아니면 맨 뒤로 보낸다
             queue.append(front)
 
-    print(f"{queue[-1]}>")
+    print(f"{queue[-1]}>")  # 마지막 사람 출력
 
 
 if __name__ == "__main__":
