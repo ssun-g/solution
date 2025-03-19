f989f2b3f139af264a026946a3b3be3212c3ade3
Jinsung Lee <ssung38477@gmail.com>
2025-03-20 00:41:42 +0900

    (250320) BOJ-2661

---
+++ BOJ/python/2661.py
@@ -0,0 +1,47 @@
+# import sys
+
+# input = sys.stdin.readline
+
+
+# def is_repeat(seq):
+#     max_repeat_len = len(seq) // 2  # 최대로 반복 가능한 부분 수열의 길이
+    
+#     for i in range(1, max_repeat_len + 1):
+#         if seq[-i:] == seq[-i*2:-i]:
+#             return True
+
+#     return False
+        
+
+# def make_good_seq(seq):
+#     global N
+    
+#     # 중복 부분 수열이 있는 경우 False 리턴
+#     if is_repeat(seq):
+#         return False
+    
+#     # 수열을 만들었으면 출력하고 True를 리턴하여 함수 전체가 종료 되도록 한다
+#     if len(seq) == N:
+#         print(seq)
+#         return True
+        
+#     for i in range(1, 4):
+#         if make_good_seq(seq+str(i)):
+#             return True
+        
+#     return False
+    
+
+# def main() -> None:
+#     global N
+    
+#     N = int(input().rstrip('\n'))
+
+#     make_good_seq("")
+
+# if __name__ == "__main__":
+#     main()
+
+a = "1213123132123121312313213121321231213123132123121312"
+b = "1213123132123121312313213121321231213123132123121312"
+print(a == b)
