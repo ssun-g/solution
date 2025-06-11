a9d52f567b99fb3b69a7088b62d143bfc94d3c67
Jinsung Lee <ssung38477@gmail.com>
2025-06-11 18:49:25 +0900

    (250611) BOJ - 11650

---
+++ BOJ/python/11650.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main() -> None:
+    N = int(input().rstrip("\n"))
+    points = [list(map(int, input().rstrip("\n").split(" "))) for _ in range(N)]
+    points.sort(key=lambda x: (x[0], x[1]))
+
+    for x, y in points:
+        print(x, y)
+
+
+if __name__ == "__main__":
+    main()
